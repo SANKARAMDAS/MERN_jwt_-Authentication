4144b49c90cd7dabaf07409309334c9a268f656c
Sankaram <das95sankaram@gmail.com>
2023-06-03 19:56:05 +0530

    eighth

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path';
 dotenv.config();
 import { notFound, errorHandle} from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -15,6 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use(cookieParser());
+
+if ( process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 app.use('/api/users', userRoutes);
 
