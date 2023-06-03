48c1b52938dda3b15e69fe1979d6066cccc7150c
Sankaram <das95sankaram@gmail.com>
2023-06-03 17:53:09 +0530

    sixth commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./components/Header.jsx";
+import { Outlet } from 'react-router-dom'
+import { Container } from 'react-bootstrap';
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Container className='my-2'>
+                <Outlet />
+            </Container>
+        </>
+    );
+};
+
+export default App;
---
+++ frontend/src/screens/HomeScreens.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hero from "../components/Hero.jsx";
+
+const HomeScreens = () => {
+    return (
+        <Hero />
+    );
+};
+
+export default HomeScreens;
